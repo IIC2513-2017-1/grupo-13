5a66969f698c3ec79e54cc968f44d02e5f09066f
crab55 <caabarca1@uc.cl>
2017-04-30 16:32:50 -0300

    added fake data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+10.times do
+  password = Faker::Internet.password(6,15)
+  Player.create([{
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    mail: Faker::Internet.unique.free_email,
+    password: password,
+    password_confirmation: password,
+    age: 20,
+    position: "Delantero"
+    }])
+end
+
+
+5.times do
+  password = Faker::Internet.password(6,15)
+  Organizer.create([{
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    mail: Faker::Internet.unique.free_email,
+    password: password,
+    age: 20
+    }])
+end
