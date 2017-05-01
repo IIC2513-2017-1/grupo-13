30a02c5821d113047bfeb3cf3023461dafce4372
crab55 <caabarca1@uc.cl>
2017-04-30 21:13:37 -0300

    match not working

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,4 +1,17 @@
 class MatchesController < ApplicationController
   def index
+    @matches = Matches.all
   end
+
+  def new
+    @matches = Matches.new
+  end
+
+  def create
+    @matches = Matches.new(matches_params)
+  end
+
+  def destroy
+  end
+
 end
