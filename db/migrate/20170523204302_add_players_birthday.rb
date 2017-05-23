e7db702d1908ed4a1858d2071a6f6581d8a33356
crab55 <caabarca1@uc.cl>
2017-05-23 16:47:48 -0400

    fixed birthday migration

--- db/migrate/20170523204302_add_players_birthday.rb
+++ db/migrate/20170523204302_add_players_birthday.rb
@@ -1,6 +1,6 @@
 class AddPlayersBirthday < ActiveRecord::Migration[5.0]
-  def change
-    drop_column :players, :age
+    def change
+    remove_column :players, :age
     add_column :players, :birthday, :datetime
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170523195314) do
+ActiveRecord::Schema.define(version: 20170523204302) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,11 +63,11 @@ ActiveRecord::Schema.define(version: 20170523195314) do
     t.string   "last_name",       null: false
     t.string   "mail",            null: false
     t.string   "position",        null: false
-    t.integer  "age"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
     t.boolean  "is_organizer"
+    t.datetime "birthday"
     t.index ["mail"], name: "index_players_on_mail", unique: true, using: :btree
   end
 
