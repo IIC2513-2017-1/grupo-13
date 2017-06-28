a9336bc800f89ba3fd7cc926838f04710ba4f65f
crabarca <caabarca1@uc.cl>
2017-06-27 23:30:20 -0400

    api working on get request, fixed gitignore

--- app/controllers/api/v1/teams_controller.rb
+++ app/controllers/api/v1/teams_controller.rb
@@ -1,25 +1,26 @@
 module Api::V1
     class TeamsController < ApiController
         before_action :authenticate
+
+
         def index
-            @teams = Teams.includes(:player).all
-        end
-
+            @teams = Team.all
+        end     
+        
         def create
-            @team = @current_player.teams.build(teams_params.merge(capitan:@current_player.id))
-            return if @tweet.save
-                render json: {errors: @team.errors}, status: :unprocessable_entity
-            end
-        end
-
+            @team = @current_player.team.build(team_params.merge(capitan:@current_player.id))
+            return if @team.save
+            render json: {errors: @team.errors}, status: :unprocessable_entity
+        end     
+        
         def show
-            @teams = Teams.find(params[:id])
+            @team = Team.find(params[:id])
         end
-        
-        
 
+    
         private
         def team_params
             params.require(:team).permit(:name)
         end
+    end
 end
--- app/views/api/v1/teams/show.json.jbuilder
+++ app/views/api/v1/teams/show.json.jbuilder
@@ -1,6 +1,6 @@
 
 json.team do
-    json.href api_v1_team_url(team)
-    json.name team.name
-    json.capitan team.capitan
+    json.href api_v1_team_url(@team)
+    json.name @team.name
+    json.capitan @team.capitan
 end
