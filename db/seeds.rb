9db4d9bac08c3555c5bb44cba7419c6c40589f54
crabarca <caabarca1@uc.cl>
2017-07-03 22:14:12 -0400

    css fixes

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,11 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+def seed_image_teams(file_name)
+  File.open(File.join(Rails.root, "/app/assets/images/teams/#{file_name}.png"))
+end
+
+
 Player.delete_all
 Team.delete_all
 Teamplayer.delete_all
@@ -51,17 +56,17 @@ p2.generate_token_and_save
     birthday: Time.now.to_date - 25.years,
     position: positions.sample,
     is_organizer: false
-
     }])
 end
 
-5.times do
-  Team.create([{
-    name: Faker::GameOfThrones.city,
-    capitan: Player.all.sample.id,
-    logo: ""
-    }])
-end
+
+Team.create(name: "Palestino", capitan: Player.all.sample.id, logo: seed_image_teams("bocajuniors"))
+Team.create(name: "Barcelona", capitan: Player.all.sample.id, logo: seed_image_teams("barcelona"))
+Team.create(name: "S.Wanderers", capitan: Player.all.sample.id, logo: seed_image_teams("wanderers"))
+Team.create(name: "Real Madrid", capitan: Player.all.sample.id, logo: seed_image_teams("real_madrid"))
+Team.create(name: "Manchester", capitan: Player.all.sample.id, logo: seed_image_teams("manchester"))
+
+
 
 Player.all.collect do |player|
   Teamplayer.create([{
@@ -69,13 +74,10 @@ Player.all.collect do |player|
     team_id: Team.all.sample.id,
     }])
 end
-2.times do
-  Tournament.create([{
-    name:Faker::StarWars.planet,
-    organizer: Player.all.sample.first_name,
-    place: Faker::HarryPotter.location,
-    number_team: 4,
-    number_date: 3
 
-    }])
-end
+
+Tournament.create(name: "Champions League", organizer: Player.first.first_name, place: "Wembley", number_team: 4, number_date: 6)
+Tournament.create(name: "Copa Chile", organizer: Player.all.sample.first_name, place: "Estadio nacional", number_team: 10, number_date: 6)
+
+
+
